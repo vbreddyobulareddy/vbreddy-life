44f8f968670dad6498781c5e8868260f8abb03c2
vbreddyobulareddy <vbreddy.obulareddy@gmail.com>
2024-01-19 12:53:53 +0530

    added linked-In button

--- app/_segments/hero/index.js
+++ app/_segments/hero/index.js
@@ -30,9 +30,31 @@ export default function HeroSegment(props) {
                 confident, naturally curious, and perpetually working on
                 improving my chops one design problem at a time.
               </h2>
-              <Link href="/connect" className="font-eurostile btn btn-wide btn-primary btn-md m-4 text-white text-lg">
-                let's connect !
-              </Link>
+              <div className="flex justify-center">
+                <Link
+                  href="/connect"
+                  className="font-eurostile btn btn-wide bg-[#0077b5] btn-md m-4 text-lg text-white border-[#0077b5] hover:bg-[#0077b5] hover:border-[#0077b5]"
+                >
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    class="h-7 w-7"
+                    fill="currentColor"
+                    style={{ color: "#fff" }}
+                    viewBox="0 0 24 24"
+                  >
+                    <path d="M4.98 3.5c0 1.381-1.11 2.5-2.48 2.5s-2.48-1.119-2.48-2.5c0-1.38 1.11-2.5 2.48-2.5s2.48 1.12 2.48 2.5zm.02 4.5h-5v16h5v-16zm7.982 0h-4.968v16h4.969v-8.399c0-4.67 6.029-5.052 6.029 0v8.399h4.988v-10.131c0-7.88-8.922-7.593-11.018-3.714v-2.155z" />
+                  </svg>
+                  <div class="outline-none mr-1 mb-1 px-3 py-1 bg-transprent text-xs font-bold text-white uppercase focus:outline-none">
+                    Linked In
+                  </div>
+                </Link>
+                <Link
+                  href="/connect"
+                  className="font-eurostile btn btn-wide btn-primary btn-md m-4 text-white text-lg"
+                >
+                  let's connect !
+                </Link>
+              </div>
             </div>
           </div>
         </div>
